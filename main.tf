aa9d9726bd3d2d26f58bc5996f65fb41786470f9
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-11-09 18:07:04 -0500

    📝 docs(README.md): add clouddrive unmount command to README.md for better user guidance ✨ feat(main.tf): update azurerm_marketplace_agreement resource name to fortigate2022 and add new resource fortigate2023 for future planning

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
-resource "azurerm_marketplace_agreement" "fortigate" {
+resource "azurerm_marketplace_agreement" "fortigate2022" {
   publisher = "fortinet"
   offer     = "fortinet_fortigate-vm_v5"
   plan      = "fortinet_fg-vm_payg_2022"
 }
 
+resource "azurerm_marketplace_agreement" "fortigate2023" {
+  publisher = "fortinet"
+  offer     = "fortinet_fortigate-vm_v5"
+  plan      = "fortinet_fg-vm_payg_2023"
+}
