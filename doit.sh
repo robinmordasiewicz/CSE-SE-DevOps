c7aa7f63f64c4acd0dd2a371fc2c2765216869c4
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-11-09 23:42:51 -0500

    🆕 feat: Add new doit.sh script to import Azure Marketplace agreements 🔧 chore(main.tf): Add new resources for azurerm_marketplace_agreement for years 2019, 2020, and 2021 to manage Azure Marketplace agreements in Terraform

---
+++ doit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+
+terraform import azurerm_marketplace_agreement.fortigate /subscriptions/6dced100-9c31-416f-aed1-67e8cfc9fe5f/providers/Microsoft.MarketplaceOrdering/agreements/fortinet/offers/fortinet_fortigate-vm_v5/plans/fortinet_fg-vm 
+
+terraform import azurerm_marketplace_agreement.fortigate2019 /subscriptions/6dced100-9c31-416f-aed1-67e8cfc9fe5f/providers/Microsoft.MarketplaceOrdering/agreements/fortinet/offers/fortinet_fortigate-vm_v5/plans/fortinet_fg-vm_payg_2019
+
+terraform import azurerm_marketplace_agreement.fortigate2020 /subscriptions/6dced100-9c31-416f-aed1-67e8cfc9fe5f/providers/Microsoft.MarketplaceOrdering/agreements/fortinet/offers/fortinet_fortigate-vm_v5/plans/fortinet_fg-vm_payg_2020
+
+terraform import azurerm_marketplace_agreement.fortigate2021 /subscriptions/6dced100-9c31-416f-aed1-67e8cfc9fe5f/providers/Microsoft.MarketplaceOrdering/agreements/fortinet/offers/fortinet_fortigate-vm_v5/plans/fortinet_fg-vm_payg_2021
+
+terraform import azurerm_marketplace_agreement.fortigate2022 /subscriptions/6dced100-9c31-416f-aed1-67e8cfc9fe5f/providers/Microsoft.MarketplaceOrdering/agreements/fortinet/offers/fortinet_fortigate-vm_v5/plans/fortinet_fg-vm_payg_2022
+
+terraform import azurerm_marketplace_agreement.fortigate2023 /subscriptions/6dced100-9c31-416f-aed1-67e8cfc9fe5f/providers/Microsoft.MarketplaceOrdering/agreements/fortinet/offers/fortinet_fortigate-vm_v5/plans/fortinet_fg-vm_payg_2023
+
+
