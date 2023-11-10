72c470229ac6ce751dce5e316b121e7cf6f63af2
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-11-09 23:29:57 -0500

    🐛 fix(main.tf): remove extra quotation mark in plan value to correct syntax error 🎨 style(terraform.tf): align assignment operators for better readability

--- terraform.tf
+++ terraform.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
     }
   }
   skip_provider_registration = true
-  use_oidc = true
-  subscription_id = "6dced100-9c31-416f-aed1-67e8cfc9fe5f"
-  tenant_id = "942b80cd-1b14-42a1-8dcf-4b21dece61ba"
+  use_oidc                   = true
+  subscription_id            = "6dced100-9c31-416f-aed1-67e8cfc9fe5f"
+  tenant_id                  = "942b80cd-1b14-42a1-8dcf-4b21dece61ba"
 }
