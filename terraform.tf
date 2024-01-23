be27520e3f019dda946702a9d39aad6a07eafe67
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-01-23 08:19:36 -0500

    Fix: updating terraform

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "1.6.4"
+  required_version = "1.7.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.78.0"
+      version = "3.88.0"
     }
   }
 }
